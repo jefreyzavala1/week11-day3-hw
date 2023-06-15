ccc66948b34c8ba189652b4d93eead140989d43a
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-06-14 21:27:07 -0400

    testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -48,8 +48,18 @@ describe("Test the todos endpoints", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(todos);
   });
-});
 
+  test("It should update a todo", async () => {
+    const todo = new Todo({ title: "gym", description: "jog" });
+    await todo.save();
+
+    const response = await request(app)
+      .put(`/todos/${todo._id}`)
+      .send({ title: "study", description: "homework" });
+
+    expect(response.statusCode).toBe(200);
+  });
+});
 describe("Test the todos endpoints for edge cases", () => {
   test("It should not create a todo", async () => {
     const response = await request(app)
@@ -58,5 +68,27 @@ describe("Test the todos endpoints for edge cases", () => {
 
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toEqual("Title is required");
+  });
+
+  test("It should return no documents found", async () => {
+    const todos = [];
+    Todo.find = jest.fn().mockResolvedValue(todos);
+
+    const response = await request(app).get("/todos");
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toEqual("No todos");
+  });
+
+  test("It should not update a todo", async () => {
+    const todo = new Todo({ title: "study", description: "hw" });
+    const randomId = new mongoose.Types.ObjectId();
+    await todo.save();
+
+    const response = await request(app)
+      .put(`/todos/${randomId}`)
+      .send({ title: "study", description: "homework" });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toEqual("No todo to update");
   });
 });
